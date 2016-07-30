bf26bd07165385366d7008d193da2c8f3cff81d9
sgaoae <gaorobert@126.com>
2016-07-30 13:39:31 +0800

    target changed currently
    encountered some difficulties while trying to set up opencv for android
    so currently set up database to store the FaceDetector.Face and Image
    file data.
    When offloading and local face detection, no need to repeat

--- PictureLoader(Re)ver.1/app/src/main/java/com/example/hyouka/pictureloaderrefactored/DatabaseHelper.java
+++ PictureLoader(Re)ver.1/app/src/main/java/com/example/hyouka/pictureloaderrefactored/DatabaseHelper.java
@@ -18,54 +18,105 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.context = context;
     }
     public static final class TableDefinition{
-        public static final String TABLE_NAME = "FaceData";
-        public static final String COLUMN_NAME_1 = "Time";
-        public static final String COLUMN_TYPE_1 = "DATETIME";
-        public static final String COLUMN_NULL_1 = "NOT NULL";
-        public static final String COLUMN_NAME_2 = "File";
-        public static final String COLUMN_TYPE_2 = "TEXT";
-        public static final String COLUMN_NULL_2 = "NOT NULL";
+        public static final String IMAGE_FILE_TABLE_NAME = "ImageFileData";
+        public static final String[][] IMAGE_FILE_TABLE_COLUMNS =
+                {
+                        {"md5","TEXT","NOT NULL"},
+                        {"time","INTEGER","NOT NULL"},
+                        {"path","TEXT","NOT NULL"},
+                        {"face_num","INTEGER","NOT NULL"}
+                };
+        public static final String FACE_TABLE_NAME = "FaceData";
+        public static final String[][] FACE_TABLE_COLUMNS =
+                {
+                        {"md5", "TEXT", "NOT NULL"},
+                        {"name","TEXT", "NULL"},
+                        {"sample","INTEGER","NOT NULL"},
+                        {"eye_dis","TEXT","NOT NULL"},
+                        {"mid_point_x","TEXT","NOT NULL"},
+                        {"mid_point_y","TEXT","NOT NULL"},
+                        {"confidence","TEXT","NOT NULL"},
+                        {"euler_x","TEXT","NOT NULL"},
+                        {"euler_y","TEXT","NOT NULL"},
+                        {"euler_z","TEXT","NOT NULL"}
+                };
     }
-    private static final String COMMON_SEP = ",";
-    private static final String CREATE_TABLE_STATEMENT =
-            "CREATE TABLE "+TableDefinition.TABLE_NAME+"("+
-                    TableDefinition.COLUMN_NAME_1+" "+TableDefinition.COLUMN_TYPE_1+" "+TableDefinition.COLUMN_NULL_1+COMMON_SEP+
-                    TableDefinition.COLUMN_NAME_2+" "+TableDefinition.COLUMN_TYPE_2+" "+TableDefinition.COLUMN_NULL_2+
-                    ");";
-    private static final String DROP_TABLE_STATEMENT = "DROP TABLE IF EXISTS "+TableDefinition.TABLE_NAME;
+    private String generateCreateTableStatement(String name,String[][] columns){
+        String statement = "CREATE TABLE "+name+"(";
+        for(int i=0;i<columns.length-1;++i){
+            for(int j=0;j<columns[i].length;++j){
+                statement += columns[i][j] + " ";
+            }
+            statement += ",";
+        }
+        for(int j=0;j<columns[columns.length-1].length;++j){
+            statement += columns[columns.length-1][j] + " ";
+        }
+        statement += ");";
+        return statement;
+    }
+    private String generateDropTableStatement(String name){
+        return "DROP TABLE IF EXISTS " + name;
+    }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_STATEMENT);
+        String IMAGE_FILE_TABLE_STATEMENT =
+                generateCreateTableStatement(TableDefinition.IMAGE_FILE_TABLE_NAME,TableDefinition.IMAGE_FILE_TABLE_COLUMNS);
+        String FACE_TABLE_STATEMENT =
+                generateCreateTableStatement(TableDefinition.FACE_TABLE_NAME,TableDefinition.FACE_TABLE_COLUMNS);
+        db.execSQL(IMAGE_FILE_TABLE_STATEMENT);
+        db.execSQL(FACE_TABLE_STATEMENT);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(DROP_TABLE_STATEMENT);
+        String DROP_IMAGE_FILE_TABLE_STATEMENT = generateDropTableStatement(TableDefinition.IMAGE_FILE_TABLE_NAME);
+        String DROP_FACE_TABLE_STATEMENT = generateDropTableStatement(TableDefinition.FACE_TABLE_NAME);
+        db.execSQL(DROP_IMAGE_FILE_TABLE_STATEMENT);
+        db.execSQL(DROP_FACE_TABLE_STATEMENT);
         onCreate(db);
     }
 
-    public void insert(long time,String file){
+    public void addImageFileRecord(String md5,long time,String path,int face_num){
         ContentValues cv = new ContentValues();
-        cv.put(TableDefinition.COLUMN_NAME_1,time);
-        cv.put(TableDefinition.COLUMN_NAME_2,file);
-        getWritableDatabase().insert(TableDefinition.TABLE_NAME,TableDefinition.COLUMN_NAME_1,cv);
+        cv.put(TableDefinition.IMAGE_FILE_TABLE_COLUMNS[0][0],md5);
+        cv.put(TableDefinition.IMAGE_FILE_TABLE_COLUMNS[1][0],time);
+        cv.put(TableDefinition.IMAGE_FILE_TABLE_COLUMNS[2][0],path);
+        cv.put(TableDefinition.IMAGE_FILE_TABLE_COLUMNS[3][0],face_num);
+        getWritableDatabase().insert(TableDefinition.IMAGE_FILE_TABLE_NAME,TableDefinition.IMAGE_FILE_TABLE_COLUMNS[0][0],cv);
     }
-    public void deleteAll(){
-        getWritableDatabase().execSQL("DELETE * FROM "+TableDefinition.TABLE_NAME);
+    public void addFaceRecord(String md5,String name,int sample,String eye_dis,String mid_x,String mid_y,String confidence,String euler_x,String euler_y,String euler_z){
+        ContentValues cv  = new ContentValues();
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[0][0],md5);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[1][0],name);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[2][0],sample);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[3][0],eye_dis);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[4][0],mid_x);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[5][0],mid_y);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[6][0],confidence);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[7][0],euler_x);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[8][0],euler_y);
+        cv.put(TableDefinition.FACE_TABLE_COLUMNS[9][0],euler_z);
+        getWritableDatabase().insert(TableDefinition.FACE_TABLE_NAME,TableDefinition.FACE_TABLE_COLUMNS[0][0],cv);
     }
-    public Cursor getAll(){
-        return getReadableDatabase().rawQuery("SELECT FROM "+TableDefinition.TABLE_NAME,null);
+    public void deleteImageFileRecord(String columnName,String para){
+        String sql = "DELETE * FROM "+TableDefinition.IMAGE_FILE_TABLE_NAME+" WHERE "+columnName+"='"+para+"'";
+        getWritableDatabase().execSQL(sql);
     }
-    public Cursor getTime(){
-        return getReadableDatabase().rawQuery("SELECT "+TableDefinition.COLUMN_NAME_1+" FROM "+TableDefinition.TABLE_NAME,null);
+    public void deleteFaceRecord(String columnName,String para){
+        String sql = "DELETE * FROM "+TableDefinition.FACE_TABLE_NAME+" WHERE "+columnName+"='"+para+"'";
+        getWritableDatabase().execSQL(sql);
     }
-    public Cursor getFileDescription(){
-        return getReadableDatabase().rawQuery("SELECT "+TableDefinition.COLUMN_NAME_2+" FROM "+TableDefinition.TABLE_NAME,null);
+    public void deleteAllImageFileRecord(){
+        getWritableDatabase().execSQL("DELETE * FROM "+TableDefinition.IMAGE_FILE_TABLE_NAME);
+    }
+    public void deleteAllFaceRecord(){
+        getWritableDatabase().execSQL("DELETE * FROM "+TableDefinition.FACE_TABLE_NAME);
     }
     public void executeSqlStatement(String sql){
         getWritableDatabase().execSQL(sql);
     }
-    public Cursor get(String sql){
+    public Cursor query(String sql){
         return getReadableDatabase().rawQuery(sql,null);
     }
 }
